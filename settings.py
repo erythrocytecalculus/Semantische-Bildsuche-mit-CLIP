86deee461d29c73c1acf7d1cb62da7bb1909fbc4
Thasarathakumar Palanisamy <thasarathakumar7@gmail.com>
2025-11-09 19:21:14 +0100

    Add configuration module for database and vector parameters

---
+++ settings.py
@@ -0,0 +1,4 @@
+ANNOY_PATH = "images_vectors.ann"
+SQLITE_PATH = "images_db.sqlite"
+VECTOR_SIZE = 768  # Dimensionality of CLIP vector (depends on model)
+ANNOY_TREE_COUNT = 2  # Number of trees for building Annoy index
