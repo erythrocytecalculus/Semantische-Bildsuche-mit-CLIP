ee1def167fc7f7532cda4e9f4fff96f497ffa44e
Thasarathakumar Palanisamy <thasarathakumar7@gmail.com>
2025-12-01 18:06:10 +0100

    Delete unused files

--- settings.py
+++
@@ -1,4 +0,0 @@
-ANNOY_PATH = "images_vectors.ann"
-SQLITE_PATH = "images_db.sqlite"
-VECTOR_SIZE = 768  # Dimensionality of CLIP vector (depends on model)
-ANNOY_TREE_COUNT = 10  # Number of trees for building Annoy index
